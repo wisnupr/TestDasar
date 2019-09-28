8e1a8aa52f8d743e2602ba0192c58e3c8dfc287e
Wisnu <pwisnu.93@gmail.com>
2019-09-28 21:17:16 +0700

    1.5

---
+++ 5.py
@@ -0,0 +1,9 @@
+def countChar(string, key):
+    
+    count = string.count(key)
+    if count > 0:
+        print(count)
+    else:
+        print('Not found!')
+        
+countChar("wisnu14523", "6")
