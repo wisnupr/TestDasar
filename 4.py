8707f8694aaec71a56233f1c032cc2a79c4f6a24
Wisnu <pwisnu.93@gmail.com>
2019-10-01 23:18:21 +0700

    1.4.2

--- 4.py
+++ 4.py
@@ -3,11 +3,12 @@ def thirdHighest(text):
     y = 0
     if type(text) != type(x):
         print("Parameter should be an array!")
-    if len(text) < 3:
+    elif len(text) < 3:
         print("Minimal array length is 3!")
-    for i in text:
-        if type(i) == type(y) :
-            x.append(i)
-    print(x[3])
+    else:
+        for i in text:
+            if type(i) == type(y) :
+                x.append(i)
+        print(x[3])
                 
-thirdHighest([1,'b',3,5,'x',4])
+thirdHighest([1,9,'e',3,6])
